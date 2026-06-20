3e64c853bbb90b7c1578575f469965a80ad502a4
NOT-Aayush <aayush4373pandey@gmail.com>
2026-06-20 20:03:34 +0530

    gsap

--- src/Components/gsap.js
+++ src/Components/gsap.js
@@ -1,6 +1,5 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 console.log("GSAP Plugins", gsap.plugins);
-export { gsap, ScrollTrigger };
+export { gsap};
